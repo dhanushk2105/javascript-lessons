63b7208ad675832b6cc1b4c8c1b1c666137b2d5d
dhanushk2105 <dhanushk2105@gmail.com>
2022-04-19 18:11:55 +0530

    Updated version v4.19.1811

--- Love Caluculator.js
+++ Love Caluculator.js
@@ -6,6 +6,10 @@ loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore > 70) {
     alert("Your love score is " + loveScore + "% You love each other like Kanye loves Kanye")
-} else {
+} 
+if (loveScore > 30 && loveScore <= 70) {
     alert("Your love score is " + loveScore + "%");
+}
+if (loveScore <= 30) {
+    alert("Your love score is " + loveScore + "% You go together like Oil and Water");
 }
