5dec20f9ce58303bbf1e2f98a84b62c490ce0ea7
dhanushk2105 <dhanushk2105@gmail.com>
2022-04-19 18:08:29 +0530

    Made a simple love calculator

---
+++ Love Caluculator.js
@@ -0,0 +1,11 @@
+herName = prompt("What's her name?");
+hisName = prompt("What's his name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "% You love each other like Kanye loves Kanye")
+} else {
+    alert("Your love score is " + loveScore + "%");
+}
